425419dad9dc57415e0b8328400fd1ed7fe9aadd
Amir Nissim <amirnissim@gmail.com>
2013-11-28 11:12:15 +0200

    refactor

--- crx/content.js
+++ crx/content.js
@@ -1,14 +1,29 @@
-// Pull request pages
 
-// link to bugzilla ticket
-var regex = /bug\s(\d+)/gi;
+var bugId;
+var bugUrl;
 
-function bugzillaLinker(match, p1) {
-  return '<a href="https://bugzilla.mozilla.org/show_bug.cgi?id=' + p1 + '">' + match + '</a>';
+var BUGID_REGEX = /bug\s(\d+)/gi;
+var BUGZILLA_BASE_URL = 'https://bugzilla.mozilla.org/show_bug.cgi?id=';
+
+function parseBugId() {
+  var titleEl = document.querySelector('.discussion-topic-header .discussion-topic-title');
+
+  if (titleEl) {
+    var matches = BUGID_REGEX.exec(titleEl.innerText);
+    bugId = matches[1];
+    if (bugId) {
+      bugUrl = BUGZILLA_BASE_URL + bugId;
+    }
+  }
 }
 
-var elements = document.querySelectorAll('.discussion-topic-title');
-for (var i = elements.length - 1; i >= 0; i--) {
-  var el = elements[i];
-  el.innerHTML = el.innerHTML.replace(regex, bugzillaLinker);
+parseBugId();
+
+if (bugUrl) {
+  // link to bugzilla ticket in topic title
+  var elements = document.querySelectorAll('.discussion-topic-title');
+  for (var i = elements.length - 1; i >= 0; i--) {
+    var el = elements[i];
+    el.innerHTML = el.innerHTML.replace(BUGID_REGEX, '<a href="' + bugUrl + '">$&</a>');
+  }
 }
